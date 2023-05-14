d9ae9a33ab68fe4bace0d6a0ea520b477812e060
DiptoChakrabarty <diptochuck123@gmail.com>
2023-05-14 12:56:08 +0530

    post request with payload

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,jsonify
+from flask import Flask,jsonify,request
 
 app = Flask(__name__)
 
@@ -6,6 +6,18 @@ app = Flask(__name__)
 def home():
     return jsonify({
         "Message": "app up and running successfully"
+    })
+
+@app.route("/access")
+def access():
+    data = request.get_json()
+    name = data.get("name", "dipto")
+    server = data.get("server","server1")
+
+    message = f"User {name} received access to server {server}"
+
+    return jsonify({
+        "Message": message
     })
 
 
